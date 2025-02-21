bed31b3b44e45311ac321b0dd8f8bcb471d30fd2
kscosta <kleberscosta@hotmail.com>
2025-02-21 08:48:56 -0300

    Infrastructure | Configuração inicial do projeto

---
+++ src/main/java/com/jobtest/techmanager/TechManagerApplication.java
@@ -0,0 +1,14 @@
+package com.jobtest.techmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechManagerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TechManagerApplication.class, args);
+    }
+
+}
